31e19b52707693fa280ffabab41c51f5faf22329
feven <feventsegay6@gmail.com>
2019-04-25 15:39:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,6 +53,7 @@ def all_winter_holiday_supplies(holiday_hash)
 holiday_hash[:winter].each do |holiday, items|
   items
 end
+end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
