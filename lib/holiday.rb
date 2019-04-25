d34dff091befb383a786d91b9728d72db1b62d93
feven <feventsegay6@gmail.com>
2019-04-25 15:22:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring].each do |holiday, items|
     items << supply
     puts items
-  
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
