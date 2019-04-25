383e3426a7521277a066b49b01cd1453a24d9413
feven <feventsegay6@gmail.com>
2019-04-25 15:31:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,8 +44,10 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash.each do|holiday,items|
 holiday_supplies[:fall]=":columbus_day"
-holiday_supplies
+items << supply_array
+items
 end
 
 def all_winter_holiday_supplies(holiday_hash)
